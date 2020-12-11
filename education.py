9cc74a2c153bdd7aafdf6d67fbc63692de4d28b5
htlong77 <htlong77@gmail.com>
2020-12-11 07:58:09 +0700

    first commit

---
+++ education.py
@@ -0,0 +1,35 @@
+"""
+Education  related!!!
+"""
+class Class:
+    """Class managed!!!"""
+    def __init__(self, code):
+#subject, num_of_credits, day_of_week, class_hours, class_room
+        """Initialize the class with a class code."""
+        self.code = code
+    def set_subject(self, subject):
+        """Set the subject of the class."""
+        self.subject = subject
+
+    def set_num_of_credits(self, num_of_credits):
+        """Set the number of credits of the class."""
+        self.num_of_credits = num_of_credits
+
+    def set_granted_lecture_room(self, granted_lecture_room):
+        """Set the granted lecture room of the class."""
+        
+
+def test():
+    """Test command goes here!!!"""
+    phy3176 = Class("PHY3176")
+    phy3176.set_subject("Cấu trúc và phản ứng hạt nhân")
+    phy3176.set_num_of_credits(3)
+    #
+    print("Testing...")
+    assert(phy3176.code == 'PHY3176')
+    assert(phy3176.subject == "Cấu trúc và phản ứng hạt nhân")
+    assert(phy3176.num_of_credits == 3)
+    print("All passed. Done!!!")
+        
+if __name__ == "__main__":
+    test()
