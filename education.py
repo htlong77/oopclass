2da2fcff15f9670475841b2cf6f26a49a2f13cd6
htlong77 <htlong77@gmail.com>
2020-12-12 10:40:22 +0700

    tạm dừng chuyển sang việc khác ưu tiên hơn

--- education.py
+++ education.py
@@ -1,12 +1,26 @@
 """
 Education  related!!!
 """
+from vietnamese import *
+class Student:
+    """Student related"""
+
+    def __init__(self, student_id):
+        """Initialize new student with his/her id."""
+        self.student_id = student_id
+
+    def set_name(self, name):
+        """Set name for the student."""
+        self.name = VnFullName(name)
+
 class Class:
     """Class managed!!!"""
     def __init__(self, code):
 #subject, num_of_credits, day_of_week, class_hours, class_room
         """Initialize the class with a class code."""
         self.code = code
+        self.students = []
+        
     def set_subject(self, subject):
         """Set the subject of the class."""
         self.subject = subject
@@ -17,19 +31,56 @@ class Class:
 
     def add_granted_room(self, granted_room):
         """Add a granted room to the class."""
-        
 
-def test():
-    """Test command goes here!!!"""
+    def load_students(self, filename):
+        """Load student informations from 'filename'."""
+        ifile = open(filename, 'r')
+        data_lines = ifile.readlines()[1:] # The first line is the header row
+        for line in data_lines:
+            # remove the newline character '\n' at the end of each line
+            line = line[:-1]
+            name, date_of_birth, student_id = line.split(',')[1:]
+            new_student = Student(student_id)
+            new_student.set_name(name)
+            self.students.append(new_student)
+        ifile.close()
+
+    def sort_students_by_name(self):
+        """Sort students by name."""
+        lenlist = len(self.students)
+        for i in range(lenlist-1):
+            for j in range(i+1, lenlist):
+                iname = self.students[i].name
+                jname = self.students[j].name
+                if iname.compare(jname) > 0:
+                    self.students[i], self.students[j] = self.students[j], self.students[i]
+
+    def print_names(self):
+        """Print name of students."""
+        for student in self.students:
+            print(f"{student.name}")
+
+def test_Class():
+    "Test 'Class' class!!!"
+    print("Testing 'Class' class...")
     phy3176 = Class("PHY3176")
     phy3176.set_subject("Cấu trúc và phản ứng hạt nhân")
     phy3176.set_num_of_credits(3)
     #
-    print("Testing...")
     assert(phy3176.code == 'PHY3176')
     assert(phy3176.subject == "Cấu trúc và phản ứng hạt nhân")
     assert(phy3176.num_of_credits == 3)
     print("All passed. Done!!!")
+    phy3176.load_students('student_infos.csv')
+    print("Before sorting names.")
+    phy3176.print_names()
+    phy3176.sort_students_by_name()
+    print("After sorting names.")
+    phy3176.print_names()
+
+def test():
+    """Test command goes here!!!"""
+    test_Class()
         
 if __name__ == "__main__":
     test()
--- vietnamese.py
+++ vietnamese.py
@@ -3,6 +3,43 @@ Vietnamese related!!!
 """
 vn_lowercase=" aàáảãạăằắẳẵặâầấẩẫậbcdđeèéẻẽẹêềếểễệfghiìíỉĩịjklmnoòóỏõọôồốổỗộơờớởỡợpqrstuùúủũụưừứửữựvwxyỳýỷỹỵz"
 vn_uppercase=" AÀÁẢÃẠĂẰẮẲẴẶÂẦẤẨẪẬBCDĐEÈÉẺẼẸÊỀẾỂỄỆFGHIÌÍỈĨỊJKLMNOÒÓỎÕỌÔỒỐỔỖỘƠỜỚỞỠỢPQRSTUÙÚỦŨỤƯỪỨỬỮỰVWXYỲÝỶỸỴZ"
+class VnFullName():
+    def __init__(self, full_name):
+        """Initialize a Vietnamese full name!!!"""
+        self.full_name = full_name
+        self.standardize()
+
+    def standardize(self):
+        """Standardize Vietnamese full name!!!"""
+        import string
+        self.full_name = string.capwords(self.full_name)
+
+    def last_middle_first_name(self):
+        """Extract last/middle/first name!!!"""
+        words = self.full_name.split()
+        last_name = words[0]
+        first_name = words[-1]
+        middle_name = " ".join(words[1:-1])
+        return last_name, middle_name, first_name
+
+    def compare(self, other_full_name):
+        """Compare with other full name!!!"""
+        slast, smiddle, sfirst = self.last_middle_first_name()
+        olast, omiddle, ofirst = other_full_name.last_middle_first_name()
+        if vntoascii(sfirst) == vntoascii(ofirst):
+            if vntoascii(slast) == vntoascii(olast):
+                if vntoascii(smiddle) == vntoascii(omiddle): return 0
+                elif vntoascii(smiddle) > vntoascii(omiddle): return 1
+                else: return -1
+            elif vntoascii(slast) > vntoascii(olast): return 1
+            else: return -1
+        elif vntoascii(sfirst) > vntoascii(ofirst): return 1
+        else: return -1
+
+    def __str__(self):
+        """Refined Vietnamese full name!!!"""
+        return f"""Tên đầy đủ:{self.full_name}"""
+        
 def vntoascii(vntxt):
 # Bắt chước code cung cấp tại địa chỉ 
 # http://www.ddth.com/showthread.php/26725-C%C3%A1ch-s%E1%BA%AFp-x%E1%BA%BFp-t%C3%AAn-theo-th%E1%BB%A9-t%E1%BB%B1-A-B-C
@@ -19,19 +56,22 @@ def vntoascii(vntxt):
             vn_ascii = vn_ascii+chr(100+posinlower)
     return vn_ascii
 
-def test():
-    """Tests using assert()."""
-    print(f"{len(vn_lowercase)}-{len(vn_uppercase)}")
-    name_str = "Nga Đức Duy Ánh Anh Hằng"
-    names = name_str.split()
-    sorted_names = sorted(names, key = vntoascii)
-    print(names)
-    print(sorted_names)
-    names = ["Nguyễn Duy Thể", "Trần Thị Phương Anh", "Nguyễn Việt Anh"]
-    sorted_names = sorted(names, key = vntoascii)
-    print(names)
-    print(sorted_names)
-    print("Testing...")
+def test_VnFullName():
+    """Test VnFullName class!!!"""
+    print("Testing VnFullName class...")
+    assert(VnFullName("nguyễn duy thể") is not None)
+    assert(VnFullName("   nguyễn     duy thể  ").full_name == "Nguyễn Duy Thể")
+    assert(VnFullName("nguyễn duy  thể  ").last_middle_first_name() == ("Nguyễn", "Duy", "Thể"))
+    assert(VnFullName("   nguyễn  hoàng  ").last_middle_first_name() == ("Nguyễn", "", "Hoàng"))
+    assert(VnFullName("Vũ Trường An").compare(VnFullName("vũ trường an ")) == 0)
+    assert(VnFullName("Vũ Trường An").compare(VnFullName(" nguyễn tùng anh  ")) == -1)
+    print("All passed. Done!!!")
+    print(VnFullName("   nguyễn   jimmy  "))
+    print(VnFullName("   Trần Thị phương   anh  "))
+    
+def test_vn_ascii():
+    """Test vn_ascii function!!!"""
+    print("Testing vn_ascii function...")
     assert(vntoascii('A') == vntoascii('a'))
     assert(vntoascii('A') != vntoascii('b'))
     assert(ord(vntoascii('A')) == 101)
@@ -43,6 +83,25 @@ def test():
     assert(ord(vntoascii('Ă')) == 107)
     assert(ord(vntoascii('Ằ')) == 108)
     print("All passed. Done!!!")
+    
+def test():
+    """Tests using assert()."""
+    print(f"{len(vn_lowercase)}-{len(vn_uppercase)}")
+#
+    test_vn_ascii()
+#
+    name_str = "Nga Đức Duy Ánh Anh Hằng"
+    names = name_str.split()
+    sorted_names = sorted(names, key = vntoascii)
+    print(names)
+    print(sorted_names)
+    names = ["Nguyễn Duy Thể", "Trần Thị Phương Anh", "Nguyễn Việt Anh"]
+    sorted_names = sorted(names, key = vntoascii)
+    print(names)
+    print(sorted_names)
+#
+    test_VnFullName()
+#
     
 if __name__ == "__main__":
     """Test commands go here."""
