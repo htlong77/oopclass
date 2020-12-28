6958c908e8d1945cf30af4049ff4ac4992537d1a
htlong77 <htlong77@gmail.com>
2020-12-28 18:24:09 +0700

    về nhà thôi

--- edumanager.py
+++ edumanager.py
@@ -1,10 +1,36 @@
 """CLI for Education Manager"""
-course_code = None
+try:
+  from lxml import etree
+  print("running with lxml.etree")
+except ImportError:
+  try:
+    # Python 2.5
+    import xml.etree.cElementTree as etree
+    print("running with cElementTree on Python 2.5+")
+  except ImportError:
+    try:
+      # Python 2.5
+      import xml.etree.ElementTree as etree
+      print("running with ElementTree on Python 2.5+")
+    except ImportError:
+      try:
+        # normal cElementTree install
+        import cElementTree as etree
+        print("running with cElementTree")
+      except ImportError:
+        try:
+          # normal ElementTree install
+          import elementtree.ElementTree as etree
+          print("running with ElementTree")
+        except ImportError:
+          print("Failed to import ElementTree from any known place")
+
+database = None
 def display_infos():
-    print(f"""Current course: {course_code}""")
+    print(f"""Current database: {database}""")
     
 def display_menu():
-    print("""1. New course
+    print("""1. New database
 2. 
 3.
 0. Quit!!!
@@ -13,9 +39,7 @@ Your choice: """, end = "" )
 if __name__ == "__main__":
     """Main program"""
     choice = 1
-    print('why')
     while choice != 0:
-        print("ha ha")
         display_infos()
         display_menu()
         choice = int(input())
